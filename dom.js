cdcbb1366152592a6efe44d5deba10f7be4128bc
KUNALI BHAVARTHE <kunalibhavarthe2003@gmail.com>
2025-12-02 17:40:11 +0530

    Add DOM manipulation and logging in dom.js

---
+++ dom.js
@@ -0,0 +1,30 @@
+let head1=document.getElementById("head1")
+console.dir(head1)
+console.log(head1)
+
+console.dir(head1.tagName)
+console.log(head1.innerText);
+console.log(head1.innerHTML);
+
+
+
+let head4=document.getElementsByClassName("head2")
+console.dir(head4)
+console.log(head4)
+
+
+
+
+let tag=document.getElementsByTagName("button")
+console.dir(tag)
+console.log(tag);
+
+
+
+
+let element=document.querySelectorAll("p")
+console.log(element);
+
+
+
+
